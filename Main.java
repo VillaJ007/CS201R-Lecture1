2a31246723e4f92a07444fef6732703064d09c61
Julio <juliostarstache@gmail.com>
2025-09-06 21:55:22 -0500

    Julio V. made changes on 9/6/25 to add the rest of program.

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+//Name: Julio Villarreal
+//Date: 9/6/2025
+// Assignment 1 
+//Lecture: CS201R
+//Lab: CS201L
+//LabTime: 12:00 PM
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,21 +28,37 @@ public class Main {
     //LECTURE 1
     //add code to input a string replacing the variable 
     //words with the input value
-
+    System.out.println("Please enter your next value: ");
+    words = scanner.next();
     //print the string
+    System.out.println("You entered " + words + ".");
 
 
 
 
     //add code to input a new value for the variable
     //letter
+    System.out.println("Enter a new value: ");
+    String newValue = scanner.next();
+    newValue = newValue.toUpperCase();
+    letter = newValue.charAt(0);
+    System.out.println("You entered " + letter + ".");
+
 
     //LECTURE 2
     //if the letter entered is A or B - print "You did GREAT!"
     //if the letter is C - print "You did GOOD!"
     //if the letter is D or F - print "Try again"
     //if the letter is something else, print "Incorrect value"
-
+    if (letter == 'A'|| letter == 'B') {
+      System.out.println("You did GREAT!");
+    } else if (letter == 'C') {
+      System.out.println("You did GOOD!");
+    } else if (letter == 'D' || letter == 'F') {
+      System.out.println("Try again");
+    } else { 
+      System.out.println("Incorrect value");
+    }  
 
     //LECTURE 1
     //this is demonstrating different values
